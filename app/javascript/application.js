30910b8252d966254fb9a9429dd8f6f0336cd928
Anita Sharma <sharma.anita00001@gmail.com>
2023-10-17 18:31:54 +0545

    Navigation Menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+  var userTab = document.getElementById('user-tab');
+  var userInfo = document.getElementById('user-info');
+
+  userTab.addEventListener('click', function() {
+    userInfo.classList.toggle('visible');
+  });
+});
