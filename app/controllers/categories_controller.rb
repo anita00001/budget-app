d8ecce7258cf0bc7b82933d480127eb916da055c
Anita Sharma <sharma.anita00001@gmail.com>
2023-10-18 22:38:35 +0545

    'Add a new Category' page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,24 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.includes(:cash_flows).order(created_at: :desc).all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon).merge(user_id: current_user.id)
+  end
 end
